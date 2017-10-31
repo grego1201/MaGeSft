614e3e7aacbd1bd4fdf3b3a4010bed7d6e77e3bf
grego1201 <gregoriopatinoesteo@gmail.com>
2017-10-31 16:48:28 +0100

    prueba commit netbeans

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -18,6 +18,7 @@ public class JavaApplication2 {
         // TODO code application logic here
         
         System.out.println("Hola mundo");
+        System.out.println("Nueva linea");
     }
     
 }
