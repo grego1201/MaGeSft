c264fde363a7c98163ad9d5a031f6156c47d0934
GonzaloPF <GonzaloPF@users.noreply.github.com>
2017-10-31 17:07:20 +0100

    sin espacios de jarillo

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,69 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the ed
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-itor.
+ * and open the template in the editor.
  */
 package javaapplication2;
 
